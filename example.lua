70eb69ddb08c2a9d7f7a26ca39b90ebcae332620
eDIY <sm.ching@ediy.com.my>
2015-06-06 15:45:17 +0800

    Create example.lua

---
+++ example.lua
@@ -0,0 +1,24 @@
+#!/usr/bin/lua
+require "ubidots" -- do not put extension (.lua) after the filename
+local api_key= "your api key here"
+local deviceID="your deviceID here"
+local value = 100 -- post this value
+
+--obtain a token based on a given api_key
+get_token(api_key)
+print("Token: " .. my_token)
+
+--post data to Ubidots
+post_value(deviceID, value)
+
+--retrive last value from Ubidots
+get_value(deviceID)
+print(my_data)
+
+--retrive all data from Ubidots
+get_value(deviceID, "all")
+print(my_data)
+
+--retrive last value from Ubidots & save to a file
+get_value(deviceID, "", "data.txt")
+--data is saved in router /tmp/data.txt
