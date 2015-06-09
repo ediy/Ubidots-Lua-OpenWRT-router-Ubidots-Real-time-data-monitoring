f1e6459f1159928ceb53f66c421717363f99256a
eDIY <sm.ching@ediy.com.my>
2015-06-09 22:06:27 +0800

    Create example.sh

---
+++ example.sh
@@ -0,0 +1,26 @@
+#!/bin/sh  
+    
+api_key="your api key here"
+deviceID="your deviceID here"
+value=10 #post this value
+
+#obtain a token
+my_token=`/usr/lib/lua/ubidots.lua -token $api_key)`
+echo "Token: "$my_token
+
+#post data to Ubidots
+/usr/lib/lua/ubidots.lua "-post" $api_key $deviceID $value
+
+#retrive last value from Ubidots
+my_data=`/usr/lib/lua/ubidots.lua "-get" $api_key $deviceID`
+echo $my_data
+
+#retrive all value from Ubidots
+my_data=`/usr/lib/lua/ubidots.lua "-get" $api_key $deviceID "all"`
+echo $my_data
+
+#retrive last value from Ubidots & save to \tmp\data1.txt       
+#/usr/lib/lua/ubidots.lua "-get" $api_key $deviceID "" "data1.txt"
+
+#retrive all data from Ubidots & save to \tmp\data2.txt       
+#/usr/lib/lua/ubidots.lua "-get" $api_key $deviceID "all" "data2.txt"
