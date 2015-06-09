08e06a9ff110e1b5e14022bc115b0938dc40c9eb
eDIY <sm.ching@ediy.com.my>
2015-06-09 22:35:13 +0800

    Update example.sh

--- example.sh
+++ example.sh
@@ -11,16 +11,16 @@ echo "Token: "$my_token
 #post data to Ubidots
 /usr/lib/lua/ubidots.lua "-post" $api_key $deviceID $value
 
-#retrive last value from Ubidots
+#retrieve last value from Ubidots
 my_data=`/usr/lib/lua/ubidots.lua "-get" $api_key $deviceID`
 echo $my_data
 
-#retrive all value from Ubidots
+#retrieve all value from Ubidots
 my_data=`/usr/lib/lua/ubidots.lua "-get" $api_key $deviceID "all"`
 echo $my_data
 
 #retrive last value from Ubidots & save to \tmp\data1.txt       
 /usr/lib/lua/ubidots.lua "-get" $api_key $deviceID "" "data1.txt"
 
-#retrive all data from Ubidots & save to \tmp\data2.txt       
+#retrieve all data from Ubidots & save to \tmp\data2.txt       
 /usr/lib/lua/ubidots.lua "-get" $api_key $deviceID "all" "data2.txt"
